d8621073a689071dd7d3e042d37b7b179bc0f5c7
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2020-11-06 00:19:42 -0300

    initial commit

---
+++ src/policy.js
@@ -0,0 +1,79 @@
+'use strict';
+const axios = require('axios');
+const UF = 28888;
+const healthCareValues = {
+  "0": 0.279,
+  "1": 0.4396,
+  "2+": 0.5599
+};
+const dentalCareValues = {
+  "0": 0.12,
+  "1": 0.1950,
+  "2+": 0.2480
+};
+
+module.exports.calculateCost = ({ age, childs }, hasDentalCare, companyPercentage) => {
+  let cost = 0;
+
+  switch (childs) {
+    case 0:
+      cost = hasDentalCare ? UF * dentalCareValues[0] : 0;
+      cost = cost + (UF * healthCareValues[0]);
+      break;
+    case 1:
+      cost = hasDentalCare ? UF * dentalCareValues[1] : 0;
+      cost = cost + (UF * healthCareValues[1]);
+      break;
+    default:
+      cost = hasDentalCare ? UF * dentalCareValues["2+"] : 0;
+      cost = cost + (UF * healthCareValues["2+"]);
+      break;
+  }
+
+  if (age > 65) {
+    return {
+      cost: 0,
+      copay: cost,
+    }
+  }
+
+  return {
+    cost: cost * (companyPercentage / 100),
+    copay: cost * ((100 - companyPercentage) / 100)
+  };
+}
+
+module.exports.getPolicy = async event => {
+  const { data } = await axios.get('https://dn8mlk7hdujby.cloudfront.net/interview/insurance/policy');
+  const { success, policy: { workers, has_dental_care: hasDentalCare, company_percentage: companyPercentage } } = data;
+
+  if (!success) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Something went wrong.',
+        data
+      })
+    };
+  }
+
+  const results = workers.map(worker => {
+    return {
+      worker,
+      cost: this.calculateCost(worker, hasDentalCare, companyPercentage)
+    }
+  })
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      hasDentalCare,
+      companyPercentage,
+      results
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
